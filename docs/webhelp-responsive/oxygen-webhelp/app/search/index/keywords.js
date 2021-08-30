7dbf95306922b9f24b3509d421ab2217a1432581
Zucker, Joe <Joe.Zucker@pega.com>
2021-08-30 13:27:45 -0700

    Updated sample output to include MD

--- docs/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
+++ docs/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -1,13 +1,14 @@
-define(function() {var keywords=[{w:"cookie",p:["p0","p3","p5"]},{w:"chocolate",p:["p1","p3","p5","p7"]},{w:"chip",p:["p1","p3","p5","p7"]},{w:"definition",p:["p2"]},{w:"Nestle",p:["p3","p5","p7"]},{w:"ingredient",p:["p4"]},{w:"Original",p:["p5"]},{w:"Ingredients",p:["p5"]},{w:"making",p:["p6","p7"]},{w:"Cookies",p:["p7"]}];
+define(function() {var keywords=[{w:"cookie",p:["p0","p3","p6"]},{w:"chocolate",p:["p1","p3","p6","p8"]},{w:"chip",p:["p1","p3","p6","p8"]},{w:"definition",p:["p2"]},{w:"Nestle",p:["p3","p6","p8"]},{w:"Sushi",p:["p4"]},{w:"Basics",p:["p4"]},{w:"ingredient",p:["p5"]},{w:"Original",p:["p6"]},{w:"Ingredients",p:["p6"]},{w:"making",p:["p7","p8"]},{w:"Cookies",p:["p8"]}];
 var ph={};
 ph["p0"]=[0];
 ph["p1"]=[1, 2];
 ph["p2"]=[3];
 ph["p3"]=[4, 1, 2, 0];
-ph["p4"]=[5];
-ph["p5"]=[6, 4, 1, 2, 0, 7];
-ph["p6"]=[8];
-ph["p7"]=[8, 4, 1, 2, 9];
+ph["p4"]=[5, 6];
+ph["p5"]=[7];
+ph["p6"]=[8, 4, 1, 2, 0, 9];
+ph["p7"]=[10];
+ph["p8"]=[10, 4, 1, 2, 11];
      return {
          keywords: keywords,
          ph: ph
--- docs/webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
+++ docs/webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,4 +1,4 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
 define(function () {
-return {0:-1,1:0,2:0};
+return {0:-1,3:0,2:0};
 });
